23d991ad893263b2d4961d2f70cea24beea5aff9
Boardonly <42874416+Boardonly@users.noreply.github.com>
2018-09-16 13:41:30 +0300

    home work

---
+++ lesson_9/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+console.log('"Вадим Захаренко"');
+
+let a = 19;
+let b = 8;
+
+
+if (a > b){
+	console.log(a);
+} else {
+	console.log('no');
+}
+
+
+
+if (a < b){
+	console.log(a);
+} else{
+	console.log('no')
+}
+
+console.log (a + b);
+
+console.log (a % b);
+
+
+if (a > b){
+	console.log((a - b) > 10 ? a + 10 : b);
+} else { 
+	console.log(9);
+}
