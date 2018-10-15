76154cf58280c05f62b31dd70e638688aa417594
Boardonly <42874416+Boardonly@users.noreply.github.com>
2018-10-15 23:01:05 +0300

    Add files via upload

--- lesson_17/index.js
+++ lesson_17/index.js
@@ -1,24 +1,28 @@
 'use strict';
-
+const createHello = document.createElement('span');
+createHello.innerText = `Привет) Введите дату в формате: ММ-ДД-ГГГГ.`
 const createInput = document.createElement('input');
 createInput.setAttribute('type', 'text')
 createInput.setAttribute('id', 'dateInput')
 const createText = document.createElement('span');
 createText.setAttribute('id', 'text');
-document.body.prepend(createInput, createText) ;
+document.body.prepend(createHello,createInput, createText) ;
 
 let today = Date.now();
+console.log(today);
 let input = document.getElementById('dateInput');
 input.addEventListener('change', () => {
-    let inputDate = document.getElementById('dateInput').value.split('-');
+    let inputDate = document.getElementById('dateInput').value;
+    console.log(inputDate);
     let dayx = Date.parse(inputDate);
+    console.log(dayx);
     let diff = 0;
     if (dayx <= today){
         let diff = Math.floor((today - dayx) / 1000 / 60 / 60 / 24);
         document.getElementById('text').innerText = `
             Дней с этой даты: ${diff}`;
     } else {
-        let diff = Math.floor((dayx - today) / 1000 / 60 / 60 / 24);
+        let diff = 1 + Math.floor((dayx - today) / 1000 / 60 / 60 / 24);
         document.getElementById('text').innerText = `
             Дней до этой даты: ${diff}`;
     }
