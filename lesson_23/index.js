e8a27bc02241fe6e76896d653410ac713d307b52
Boardonly <42874416+Boardonly@users.noreply.github.com>
2018-11-16 14:56:35 +0200

    Add files via upload

--- lesson_23/index.js
+++ lesson_23/index.js
@@ -1,281 +1,88 @@
+'use strict'
+// Создать форму “Синонимы”, сделать ее доступной для просмотра на gh-pages вашего репозитория
+resetInput();
 
-import MaskInput from 'mask-input';
-import {UIController} from './ui_controller';
+const text = document.getElementById('word');
+const inpHeight = document.getElementById('height');
+const inpWidth = document.getElementById('width');
+const button = document.getElementById('submit');
+const divResult = document.getElementById('result');
+const preloader = document.getElementById('preloader');
 
-const uiController = new UIController;
-const maskInput = new MaskInput(document.querySelector('.js-input-selector'), {
-  mask: '0000-0000-0000-0000',
-  alwaysShowMask: true,
-  maskChar: '_',
-  onChange(){
-	  if(this.inputCard.value.match(/^(\d{4}-){3}\d{4}/)){
-			UIController.validInput(this.inputCard);
-		} else {
-			UIController.invalidInput(this.inputCard);
-		}
-		UIController.validation()
-	}
-});
-
-this.inputName.addEventListener('change', ()=>{
-	if (this.inputName.value.trim().length > 2){
-		UIController.validInput(this.inputName);
-	} else {
-		UIController.invalidInput(this.inputName);
-	}
-	UIController.validation();
+inpHeight.addEventListener('change', ()=>{
+    if(inpHeight.value < 50 || inpHeight.value > 500){
+        invalidInput(inpHeight);
+    } else {
+        validInput(inpHeight);
+    }
+    validation()
 })
 
-this.button.addEventListener('click', ()=>{
-	let cardNumber = this.inputCard.value.split('-').join('');
-	console.log(cardNumber);
-    UIController.getCardInfo(cardNumber);
+inpWidth.addEventListener('change', ()=>{
+    if(inpWidth.value < 50 || inpWidth.value > 500){
+        invalidInput(inpWidth);
+    } else {
+        validInput(inpWidth);
+    }
+    validation()
 })
 
+text.addEventListener('change', ()=>{
+    if (text.value.trim().length < 2){
+        invalidInput(text);
+    } else {
+        validInput(text);
+    }
+    validation();
+})
+
+button.addEventListener('click', ()=>{
+    datamuse(text.value);
+})
+
+function validInput(name){
+    name.className = "green";  
+    name.valid = true;
+}
+function invalidInput(name){
+    name.value = null;
+    name.className = "red";
+    name.valid = false;
+}
+function validation(){
+if(text.valid && inpHeight.valid && inpWidth.valid){
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
+function resetInput(){
+    text.value = null;
+    inpHeight.value = null;
+    inpWidth.value = null;
+    button.disabled = true;
+    text.valid = false;
+    inpHeight.valid = false;
+    inpWidth.valid = false;
+}
+function datamuse (value){
+    preloader.className = 'preloader';
+    divResult.className = 'hide';
+    return fetch(`https://api.datamuse.com/words?ml=${value}`)
+        .then(res => res.json())
+        .then(res => toimg(res))
+        .then(() =>{
+            preloader.className = 'hide';
+            divResult.className = 'result';
+            resetInput();
+        })
+ };
+
+function toimg(words) {
+        for(let i = 0; i < 10; i += 1){
+        let img = document.createElement('img');
+        img.src = `https://dummyimage.com/${inpHeight.value}x${inpWidth.value}/ffffff/000000&text=${words[i].word}`;
+        divResult.appendChild(img);
+    }   
+}
 
-// class CardData{
-//     constructor(card){
-//         this.card = card;
-//     }
-//     getCardInfo(card){
-//         preloader.className = 'preloader';
-//         divResult.className = 'hide';
-//         return fetch(`https://api.bincodes.com/cc/?format=json&api_key=d96ca493f5be297f8c304a87edcdf6a8&cc=${this.card}`)
-//             .then(res => res.json())
-//             .then(res => console.log(res))
-//             .then(res => toTable(res))
-//             .then(() =>{
-//                 preloader.className = 'hide';
-//                 divResult.className = 'result';
-//                 resetInput();
-//             })
-//     };
-// }
-
-
-
-// class UIController{
-//     constructor(data){
-//         this.data = data;
-//         this.inputName = document.getElementById('name');
-//         this.inputCard = document.getElementById('card');
-//         this.button = document.getElementById('submit');
-//         this.divResult = document.getElementById('result');
-//         this.tabResult = document.getElementById('table_result');
-//         this.preloader = document.getElementById('preloader');
-//     }
-//         toTable() {
-//             const keys = Object.keys(this.data);
-//             const values = Object.values(this.data);
-//             const table = document.getElementById('table_result');
-//             for (let i = 0; i < keys.length; i += 1) {
-// 				const tr = document.createElement('tr');
-				
-//                 const td_key = document.createElement('td');
-//                 td_key.innerText = `${keys[i]}`;
-//                 tr.append(td_key);
-//                 const td_value = document.createElement('td');
-//                 td_value.innerText = `${values[i]}`;
-//                 tr.append(td_value);
-//                 table.append(tr);
-//             }
-//         };
-//         onChange(){
-//             if(inputCard.value.match(/^(\d{4}-){3}\d{4}/)){
-//                   validInput(inputCard);
-//               } else {
-//                   invalidInput(inputCard);
-//               }
-//               validation()
-//           }
-//         validInput(name){
-//             name.className = "green";  
-//             name.valid = true;
-//         }
-//         invalidInput(name){
-//             name.className = "red";
-//             name.valid = false;
-//         }
-        
-//         validation(){
-//             if(inputName.valid && inputCard.valid){
-//                     button.disabled = false;
-//                 } else {
-//                     button.disabled = true;
-//             }
-//         }  
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-// // const inputName = document.getElementById('name');
-// // const inputCard = document.getElementById('card');
-// // const button = document.getElementById('submit');
-// // const divResult = document.getElementById('result');
-// // const tabResult = document.getElementById('table_result');
-// // const preloader = document.getElementById('preloader');
-// // const controller = new UIController();
-// // const maskInput = new MaskInput(document.querySelector('.js-input-selector'), {
-// // 	mask: '0000-0000-0000-0000',
-// // 	alwaysShowMask: true,
-// // 	maskChar: '_',
-// // 	onChange(){
-// // 		if(inputCard.value.match(/^(\d{4}-){3}\d{4}/)){
-// // 			  validInput(inputCard);
-// // 		  } else {
-// // 			  invalidInput(inputCard);
-// // 		  }
-// // 		  validation()
-// // 	  }
-// //   });
-  
-// //   inputName.addEventListener('change', ()=>{
-// // 	  if (inputName.value.trim().length > 2){
-// // 		  validInput(inputName);
-// // 	  } else {
-// // 		  invalidInput(inputName);
-// // 	  }
-// // 	  validation();
-// //   })
-  
-// //   button.addEventListener('click', ()=>{
-// // 	  let cardNumber = inputCard.value.split('-').join('');
-// // 	  console.log(cardNumber);
-// // 	  getCardInfo(cardNumber);
-// //   })
-  
-// //   function validInput(name){
-// // 	  name.className = "green";  
-// // 	  name.valid = true;
-// //   }
-// //   function invalidInput(name){
-// // 	  name.className = "red";
-// // 	  name.valid = false;
-// //   }
-  
-// //   function validation(){
-// // 	  if(inputName.valid && inputCard.valid){
-// // 			  button.disabled = false;
-// // 		  } else {
-// // 			  button.disabled = true;
-// // 	  }
-// //   }
-  
-  
-  
-  
-// //   function getCardInfo(card){
-// // 	  preloader.className = 'preloader';
-// // 	  divResult.className = 'hide';
-// // 	  return fetch(`https://api.bincodes.com/cc/?format=json&api_key=d96ca493f5be297f8c304a87edcdf6a8&cc=${card}`)
-// // 		  .then(res => res.json())
-// // 		  .then(res => console.log(res))
-// // 		  .then(res => toTable(res))
-// // 		  .then(() =>{
-// // 			  preloader.className = 'hide';
-// // 			  divResult.className = 'result';
-// // 			  resetInput();
-// // 		  })
-// //    };
-  
-  
